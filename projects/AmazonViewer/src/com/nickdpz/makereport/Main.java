9941208ba3d6b066b4c7e0d0602fee0a36afad53
nickdpz <ndpastranzamora@gmail.com>
2022-11-26 22:06:03 -0500

    :sparkles: Add folder projects

---
+++ projects/AmazonViewer/src/com/nickdpz/makereport/Main.java
@@ -0,0 +1,20 @@
+package com.nickdpz.makereport;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Report report = new Report();
+		report.setNameFile("reporte");
+		report.setExtension("txt");
+		report.setTitle(":: REPORTE:: \n");
+		String content = report.getTitle();
+		for (int i = 0; i < 5; i++) {
+			content += "\nReporte : " + i;
+		}
+		report.setContent(content);
+		report.makeReport();
+
+	}
+
+}
---
+++ projects/AmazonViewer/src/com/nickdpz/util/AmazonUtil.java
@@ -0,0 +1,37 @@
+package com.nickdpz.util;
+
+import java.util.Scanner;
+
+public class AmazonUtil {
+
+	public static int validateUserResponseMenu(int min, int max) {
+		try (
+				Scanner sc = new Scanner(System.in)) {
+
+			while (!sc.hasNextInt()) {
+				sc.next();
+				System.out.println("No ingresaste una opción válida1");
+				System.out.println("Intenta otra vez1");
+			}
+
+			int response = sc.nextInt();
+
+			// Validar rango de respuesta
+			while (response < min || response > max) {
+				// Solicitar de nuevo la respuesta
+				System.out.println("No ingresaste una opción válida");
+				System.out.println("Intenta otra vez");
+
+				while (!sc.hasNextInt()) {
+					sc.next();
+					System.out.println("No ingresaste una opción válida");
+					System.out.println("Intenta otra vez");
+				}
+				response = sc.nextInt();
+			}
+			System.out.println("Tu Respuesta fue: " + response + "\n");
+			return response;
+		}
+	}
+
+}
